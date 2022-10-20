364b5fe71fec42c0cc6008326d82041875bfcccf
Leboullenger <luisleboullenger@gmail.com>
2022-10-20 17:25:52 +0200

    Patch vr bug + cauldron and items place

--- Assets/Scripts/Cauldron.cs
+++ Assets/Scripts/Cauldron.cs
@@ -4,38 +4,9 @@ using UnityEngine;
 
 public class Cauldron : MonoBehaviour
 {
-    [SerializeField] Transform Mushroom1Spot;
-    [SerializeField] GameObject Mushroom1Prefab;
-
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag.Equals("Object"))
-        {
-            if (collision.transform.name == "Mushroom 1")
-            {
-
-            }
-            if (collision.transform.name == "Mushroom 2")
-            {
-
-            }
-            if (collision.transform.name == "Mushroom 3")
-            {
-
-            }
-            if (collision.transform.name == "LightingPlant1")
-            {
-
-            }
-            if (collision.transform.name == "PlantEx2")
-            {
-
-            }
-            if (collision.transform.name == "bat_wing2")
-            {
-
-            }
-
-        }
+        if (collision.transform.tag == "Object")
+            collision.transform.localPosition = new Vector3(0, 0, 0);
     }
 }
